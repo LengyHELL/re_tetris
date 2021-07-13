6c39dc1ae241d7a3ced89e2e875edba8b41d2422
LengyHELL <ubi19998@gmail.com>
2021-07-13 17:27:10 +0200

    Extending and testing Game class

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,93 @@
 #include "Block.hpp"
 #include "Board.hpp"
 
+class Game {
+  Board board;
+  Block active_block = Block('t');
+
+  float step = 500; //ms
+  float step_current = step;
+  float step_timer = 0;
+
+  bool left_lock = true;
+  bool right_lock = true;
+  bool up_lock = true;
+  bool down_lock = true;
+
+  char types[7] = {'o', 'i', 'j', 'l', 't', 's', 'z'};
+
+public:
+
+  void update(const Engine& engine) {
+    Coord x_movement(0, 0);
+    Coord y_movement(0, 0);
+    bool rotation = false;
+
+    step_timer += engine.get_ft();
+
+    if (engine.keyboard_state[SDL_SCANCODE_LEFT] && !left_lock) {
+      left_lock = true;
+      x_movement += Coord(-1, 0);
+    }
+    else if (!engine.keyboard_state[SDL_SCANCODE_LEFT]) { left_lock = false; }
+
+    if (engine.keyboard_state[SDL_SCANCODE_RIGHT] && !right_lock) {
+      right_lock = true;
+      x_movement += Coord(1, 0);
+    }
+    else if (!engine.keyboard_state[SDL_SCANCODE_RIGHT]) { right_lock = false; }
+
+    if (engine.keyboard_state[SDL_SCANCODE_UP] && !up_lock) {
+      up_lock = true;
+      rotation = true;
+    }
+    else if (!engine.keyboard_state[SDL_SCANCODE_UP]) { up_lock = false; }
+
+    if (engine.keyboard_state[SDL_SCANCODE_DOWN] && !down_lock) {
+      down_lock = true;
+      step_current = step / 4;
+      step_timer = step_current;
+    }
+    else if (!engine.keyboard_state[SDL_SCANCODE_DOWN]) {
+      down_lock = false;
+      step_current = step;
+    }
+
+    if (step_timer >= step_current) {
+      step_timer -= step_current;
+      y_movement += Coord(0, 1);
+    }
+
+    if (rotation) {
+      active_block.rotate();
+      if (!board.check_block(active_block)) {
+        for (int i = 0; i < 3; i++) { // this is not ok
+          active_block.rotate();
+        }
+      }
+    }
+
+    active_block.move(x_movement);
+    if (!board.check_block(active_block)) {
+      active_block.move(x_movement * -1);
+    }
+
+    active_block.move(y_movement);
+    if (!board.check_block(active_block)) {
+      active_block.move(y_movement * -1);
+      board.set_block(active_block);
+      active_block = Block(types[rand() % 7]);
+    }
+
+
+  }
+
+  void draw(const Engine& engine) {
+    board.draw(engine);
+    board.draw_block(engine, active_block);
+  }
+};
+
 int main(int argc, char** argv) {
   // Initializing engine
   srand(time(NULL));
@@ -30,43 +117,14 @@ int main(int argc, char** argv) {
 
   // Test
   //...
-  Board board;
-  Block block('t');
-  bool left_lock = true;
-  bool right_lock = true;
-  bool down_lock = true;
-  bool up_lock = true;
+  Game game;
   // End Test
 
   while(!engine.get_exit()) {
     engine.update_inputs();
 
-    board.draw(engine);
-    board.draw_block(engine, block);
-
-    if (engine.keyboard_state[SDL_SCANCODE_LEFT] && !left_lock) {
-      left_lock = true;
-      block.move(Coord(-1, 0));
-    }
-    else if (!engine.keyboard_state[SDL_SCANCODE_LEFT]) { left_lock = false; }
-
-    if (engine.keyboard_state[SDL_SCANCODE_RIGHT] && !right_lock) {
-      right_lock = true;
-      block.move(Coord(1, 0));
-    }
-    else if (!engine.keyboard_state[SDL_SCANCODE_RIGHT]) { right_lock = false; }
-
-    if (engine.keyboard_state[SDL_SCANCODE_DOWN] && !down_lock) {
-      down_lock = true;
-      block.move(Coord(0, 1));
-    }
-    else if (!engine.keyboard_state[SDL_SCANCODE_DOWN]) { down_lock = false; }
-
-    if (engine.keyboard_state[SDL_SCANCODE_UP] && !up_lock) {
-      up_lock = true;
-      block.rotate();
-    }
-    else if (!engine.keyboard_state[SDL_SCANCODE_UP]) { up_lock = false; }
+    game.update(engine);
+    game.draw(engine);
 
     engine.draw_text(std::to_string(engine.get_fps()) + " fps", Coord(5, 5), {255, 0, 0, 0}, 16);
     engine.render();
