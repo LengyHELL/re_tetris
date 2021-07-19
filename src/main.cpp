0c4d046adc0752ee52d627d1a891bbda6250a223
LengyHELL <ubi19998@gmail.com>
2021-07-19 13:54:05 +0200

    Adding restart prompt

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,10 @@ int main(int argc, char** argv) {
   engine.load_image("img/basic_style.png");
 
   // Creating menu elements
-  //...
+  Frame yesno("img/basic_style.png", Rect(150, 75, 150, 75));
+  TextButton yes("img/basic_style.png", Rect(160, 115, 40, 25), "Yes", 16, {0, 0, 0, 0});
+  TextButton no("img/basic_style.png", Rect(250, 115, 40, 25), "No", 16, {0, 0, 0, 0});
+  bool exit = false;
 
   // Creating game objects
   //...
@@ -32,11 +35,29 @@ int main(int argc, char** argv) {
   Game game;
   // End Test
 
-  while(!engine.get_exit()) {
+  while(!engine.get_exit() && !exit) {
     engine.update_inputs();
 
     game.update(engine);
     game.draw(engine);
+
+    if (game.is_over()) {
+      yesno.draw(engine);
+      yes.draw(engine);
+      no.draw(engine);
+      engine.draw_text("Restart?", Coord(180, 85), {0, 0, 0, 0}, 16);
+
+      yesno.update(engine);
+      yes.update(engine);
+      no.update(engine);
+
+      if (yes.selected) {
+        game = Game();
+      }
+      else if (no.selected) {
+        exit = true;
+      }
+    }
 
     engine.draw_text(std::to_string(engine.get_fps()) + " fps", Coord(5, 5), {255, 0, 0, 0}, 16);
     engine.render();
